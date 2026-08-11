35bc1a730eec1a23f2e4ffac29aaed94c85587f6
instagrmauwu <yuassine605@gmail.com>
2026-08-11 23:33:11 +0200

    Create __init__.py

---
+++ modules/exfil/__init__.py
@@ -0,0 +1,5 @@
+from .collector import ExfilCollector
+from .dns_channel import DNSChannel
+from .http_channel import HTTPChannel
+from .relay import RelayChain, RelayError
+from .stego_png import capacity, embed_payload, extract_payload, read_png, write_png
